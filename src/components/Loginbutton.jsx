f9b397d8303a9c134f4c7770597a0a24b0fd93cb
pudders <ericpudney@gmail.com>
2024-03-19 14:07:23 +0100

    added radio buttons to select login or register

--- src/components/Loginbutton.jsx
+++ src/components/Loginbutton.jsx
@@ -2,10 +2,13 @@ import { GlobalContext } from "../GlobalContext";
 import { useContext, useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+
 
 export default function Loginbutton(){
     const {login, setLogin} = useContext(GlobalContext)
     const [showModal, setShowModal] = useState(false)
+    const [selectedValue, setSelectedValue] = useState("login")
 
 
     const handleClose = () => setShowModal(false);
@@ -23,8 +26,37 @@ export default function Loginbutton(){
             <Modal.Title>Log in</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <input type="email" className="form-control" placeholder="Username" aria-label="Username" required/>
-                <input type="password" className="form-control" placeholder="Password" aria-label="Password" required/>
+                <Form>
+                <div key={`inline-radio`} className="mb-3">
+            <Form.Check
+            inline
+            label="Log in"
+            name="login"
+            type="radio"
+            id="login"
+            checked={ 
+                selectedValue === 
+                "login"
+            } 
+            onClick={() => setSelectedValue("login")}
+          />
+          <Form.Check
+            inline
+            label="Register"
+            name="register"
+            type="radio"
+            id="register"
+            checked={ 
+                selectedValue === 
+                "register"
+            } 
+            onClick={() => setSelectedValue("register")}
+
+          />
+                </div>
+                <input type="email" name="email" autoComplete="email" className="form-control" placeholder="Username" aria-label="Username" required/>
+                <input type="password" name="password" className="form-control" placeholder="Password" aria-label="Password" required/>
+                </Form>
             </Modal.Body>
             <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
