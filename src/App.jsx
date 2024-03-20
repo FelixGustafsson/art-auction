fb8f2de3f96a8e07c4ea7277f0a78a4cc2d2a5b1
pudders <ericpudney@gmail.com>
2024-03-20 11:49:21 +0100

    fixed merge errors

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer.jsx';
 import Home from './routes/Home.route.jsx';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from "./routes/Home.route.jsx"
 import Profile from './routes/Profile.route.jsx'
 import Loginbutton from './components/Loginbutton.jsx'
 import { GlobalProvider } from './GlobalContext.jsx'
