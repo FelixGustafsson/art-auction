37b74a86922d45480f1ad824a268a221274b74d1
pudders <ericpudney@gmail.com>
2024-03-21 15:30:36 +0100

    Added create auction form

--- src/routes/Profile.route.jsx
+++ src/routes/Profile.route.jsx
@@ -12,6 +12,8 @@ export default function Profile() {
     const [userInfo, setUserInfo] = useState([])
     const [showEditForm, setShowEditForm] = useState(false)
     const [showSuccessModal, setShowSuccessModal] = useState(false)
+    const [showAuctionForm, setShowAuctionForm] = useState(false)
+    const [successText, setSuccessText] = useState("")
     const currentUser = login
     const redirect = useNavigate()
     
@@ -26,7 +28,7 @@ export default function Profile() {
         fetchUser();
       }, []);
 
-    const handleSubmit = async (event) => {
+    const handleSubmitInfo = async (event) => {
         event.preventDefault()
         const form = event.currentTarget.elements
         const newUserInfo = {
@@ -43,27 +45,56 @@ export default function Profile() {
             },
             body: JSON.stringify(newUserInfo)
         })
-        if (response.status === 200){
+        if (response.status === 200) {
             setShowEditForm(false)
+            setSuccessText("User information updated.")
             setShowSuccessModal(true)
             const updatedUser = await fetch (`http://localhost:8000/users/${userInfo.id}`)
             const updatedUserInfo = await updatedUser.json()
             setUserInfo(updatedUserInfo)
         }
-        
+    }
+
+    const handleSubmitAuction = async(event) => {
+        event.preventDefault()
+        const form = event.currentTarget.elements
+        const newAuctionInfo = {
+            title: form.title.value,
+            description: form.description.value,
+            artist: form.artist.value,
+            startingBid: form.startingBid.value,
+            image: form.image.value,
+            auctionEnds: form.auctionEnds.value
+        }
+        const response = await fetch("http://localhost:8000/items", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newAuctionInfo)
+        })
+        if (response.status === 201) {
+            setShowEditForm(false)
+            setSuccessText("New auction created.")
+            setShowSuccessModal(true)
+        }
+        else{
+        console.log(response.status)
+        }
     }
 
     return <>
         <h1>Welcome {userInfo.name}</h1>
         <button className="btn btn-primary" onClick={()=>setShowEditForm(true)}>Edit account info</button>
         <button className="btn btn-secondary" onClick={()=>{setLogin(null); redirect("/")}}>Logout</button>
+        <button type="button" className="btn btn-success" onClick={()=>setShowAuctionForm(true)}>Create new auction</button>
         <button onClick={()=>(console.log(userInfo))}>show user info</button>
         <Modal show={showEditForm} onHide={()=>setShowEditForm(false)} animation={false}>
             <Modal.Header closeButton>
               <Modal.Title>Edit account information</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-            <Form onSubmit={handleSubmit} return="false">
+            <Form onSubmit={handleSubmitInfo} return="false">
                 <input type="text" name="username" className="form-control mb-2" placeholder="Username" aria-label="username" required/>
                 <input type="text" name="name" className="form-control mb-2" placeholder="Name" aria-label="name" required/>
                 <input type="text" name="lastname" className="form-control mb-2" placeholder="Surname" aria-label="lastname" required/>
@@ -80,6 +111,31 @@ export default function Profile() {
             <Modal.Footer>
             </Modal.Footer>
           </Modal>           
-        <SuccessModal showSuccessModal={showSuccessModal} successText={"User information updated!"} dismiss={()=>setShowSuccessModal(false)}/> 
+
+          <Modal show={showAuctionForm} onHide={()=>setShowAuctionForm(false)} animation={false}>
+            <Modal.Header closeButton>
+              <Modal.Title>Create a new auction</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+            <Form onSubmit={handleSubmitAuction} return="false">
+                <input type="text" name="title" className="form-control mb-2" placeholder="Title" aria-label="title" required/>
+                <input type="text" name="description" className="form-control mb-2" placeholder="Description" aria-label="description" required/>
+                <input type="text" name="artist" className="form-control mb-2" placeholder="Artist" aria-label="artist" required/>
+                <input type="number" name="startingBid" className="form-control mb-2" placeholder="Starting price" aria-label="startingBid" required/>
+                <input type="text" name="image" className="form-control mb-2" placeholder="Image link" aria-label="image" required/>
+                <input type="datetime-local" name="auctionEnds" className="form-control mb-2" placeholder="End date" aria-label="auctionEnds" required/>
+            <Button variant="secondary" className="my-3" onClick={()=>setShowAuctionForm(false)}>
+            Cancel
+            </Button>
+            <Button variant="primary" className="my-3 ms-2" type="submit">
+            Create auction
+            </Button>
+            </Form>
+            </Modal.Body>
+            <Modal.Footer>
+            </Modal.Footer>
+          </Modal>     
+          
+        <SuccessModal showSuccessModal={showSuccessModal} successText={successText} dismiss={()=>setShowSuccessModal(false)}/> 
     </>
 }
