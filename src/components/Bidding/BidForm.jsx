37ec8756437522dbdfbce2ca3e62ae3496d0609f
VeroniccaStrand <veronicca.strand@gmail.com>
2024-03-21 20:18:41 +0100

    init

---
+++ src/components/Bidding/BidForm.jsx
@@ -0,0 +1,5 @@
+function BidForm() {
+  return <></>;
+}
+
+export default BidForm;
---
+++ src/routes/Bid.route.jsx
@@ -0,0 +1,9 @@
+function BidPage() {
+  return (
+    <div>
+      <h1>BID</h1>
+    </div>
+  );
+}
+
+export default BidPage;
