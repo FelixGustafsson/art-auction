6044282b7135a0a75ec266da2f21c561103bc735
FelixGustafsson <felixgustafssong@gmail.com>
2024-03-21 12:41:13 +0100

    add filter functionality to homepage - functionality to add: buttons redirect

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,23 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer.jsx';
 import Home from './routes/Home.route.jsx';
-import Profile from './routes/Profile.route.jsx'
-import Loginbutton from './components/Loginbutton.jsx'
-import { GlobalProvider } from './GlobalContext.jsx'
-
+import Profile from './routes/Profile.route.jsx';
+import Loginbutton from './components/Loginbutton.jsx';
+import { GlobalProvider } from './contexts/GlobalContext.jsx';
 
 function App() {
   return (
     <>
       <GlobalProvider>
-      <BrowserRouter>  
-      <Navbar/> 
-      <Loginbutton/>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/profile" element={<Profile/>}/>
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+        <BrowserRouter>
+          <Navbar />
+          <Loginbutton />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/profile' element={<Profile />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
       </GlobalProvider>
     </>
   );
--- src/components/AuctionListItem.jsx
+++ src/components/AuctionListItem.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 const AuctionListItem = ({ auction }) => {
-    return (
-        <li className='d-flex justify-content-between shadow rounded-5 p-5'>
-            <div className='me-5'>
-                <h2>{auction.title}</h2>
-                <p>{auction.description}</p>
-                <div className='d-flex flex-column'>
-                    <span>Other information:</span>
-                    <span>Starting price: £{auction.startingBid}</span>
-                    <span>Current bid: £{auction.highestBid.amount}</span>
-                    <span>Auction ends: {auction.auctionEnds}</span>
-                    <div className='my-3 d-flex gap-2'>
-                        <button className='btn btn-dark'>More info</button>
-                        <button className='btn btn-primary'>Add to favourites</button>
-                        <button className='btn btn-success'>Bid now</button>
-                    </div>
-                </div>
-            </div>
-            <img src={auction.image} className='w-50 h-auto rounded-5' />
-        </li>
-    )
-}
+  return (
+    <li className='d-flex justify-content-between shadow rounded-5 p-5'>
+      <div className='me-5'>
+        <h2>{auction.title}</h2>
+        <p>{auction.description}</p>
+        <div className='d-flex flex-column'>
+          <span>Other information:</span>
+          <span>Starting price: £{auction.startingBid}</span>
+          <span>Current bid: £{auction.highestBid.amount}</span>
+          <span>Auction ends: {auction.auctionEnds}</span>
+          <div className='my-3 d-flex gap-2'>
+            <button className='btn btn-dark'>More info</button>
+            <button className='btn btn-primary'>Add to favourites</button>
+            <button className='btn btn-success'>Bid now</button>
+          </div>
+        </div>
+      </div>
+      <img src={auction.image} className='w-50 h-auto rounded-5' />
+    </li>
+  );
+};
 
-export default AuctionListItem
+export default AuctionListItem;
