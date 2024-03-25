59c485e100e2da7a628b0951702949f69ea2c605
YacineNilsson <yacine.nilsson@gmail.com>
2024-03-25 03:26:39 +0100

    css-styling

--- src/routes/HelpCenter.route.jsx
+++ src/routes/HelpCenter.route.jsx
@@ -44,14 +44,16 @@ const HelpCenter = () => {
     return (
         <div className="d-flex flex-column mx-4">
             <h1 className="mt-4 mb-6 mx-4">Help Center</h1>
-            <div className="text-center ">
+            <div className="d-flex justify-content-around">
                 <button type='button' className='btn btn-secondary btn-lg mr-1'>Buying</button>
                 <button type='button' className='btn btn-secondary btn-lg mr-1'>Selling</button>
             </div>
             <h3 className="mx-4">FAQ</h3>
-            <div className='mx-4 mb-4 min-vw-25 max-vw-50'>
-                {/* Skicka in faqData som props till FAQ */}
-                <FAQ faqData={faqData} />
+            <div className='d-flex mx-4 mb-4 mr'>
+                <div className="w-50">
+                    {/* Skicka in faqData som props till FAQ */}
+                    <FAQ faqData={faqData} />
+                </div>
             </div>
         </div>
     );
