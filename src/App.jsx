734e6837940d427f0fcb69ec61b442766a4ed8c8
m9khaleghi <m9khaleghi@gmail.com>
2024-03-26 10:27:49 +0100

    make links functional and change styling

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
       <GlobalProvider>
         <ListingProvider>
           <BrowserRouter>
-            <AuctionSearch />
+            
             <Navbar />
             
             <Routes>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,27 @@
 import AuctionSearch from "./Search/AuctionSearch";
 import Loginbutton from "./Loginbutton";
+import { useNavigate } from "react-router-dom";
 const Navbar = () => {
+
+const navigate = useNavigate()
   return (
     
-      <nav className="navbar navbar-expand-md bg-body-tertiary m-2 d-flex justify-content-between mx-5">
+      <nav className="navbar bg-body-tertiary pd-flex justify-content-between px-5">
         
-          <img src="https://seeklogo.com/images/L/Leonardo_da_Vinci-logo-7467C5036F-seeklogo.com.png" alt="Logo" width="80" height="80" />
+          <img className="btn" onClick={navigate("/")} src="https://seeklogo.com/images/L/Leonardo_da_Vinci-logo-7467C5036F-seeklogo.com.png" alt="Logo" width="80" height="80" />
           <div className="d-flex">
-            <ul className="d-flex gap-4 list-unstyled">
+            <ul className="d-flex gap-4 list-unstyled align-items-center">
               <li className="nav-item">
                 <AuctionSearch/>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Auction list</a>
+                <a className="nav-link btn" onClick={navigate("/")}>Auction list</a>
               </li>
               <li className="nav-item">
                 <Loginbutton/>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Help</a>
+                <a className="nav-link btn" onClick={navigate("/")}>Help</a>
               </li>
             </ul>
           </div>
