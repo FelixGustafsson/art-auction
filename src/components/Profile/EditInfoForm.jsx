f8692f8b3d955b4ab2c7be816978e3bbc885406c
pudders <ericpudney@gmail.com>
2024-04-04 12:03:01 +0200

    populate edit info form with existing data

--- src/components/Profile/EditInfoForm.jsx
+++ src/components/Profile/EditInfoForm.jsx
@@ -3,12 +3,13 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { FetchContext } from '../../contexts/FetchContext';
 import { useContext } from 'react';
+import { GlobalContext } from '../../contexts/GlobalContext';
 
 
 export default function EditInfoForm({setSuccessText, userInfo, setUserInfo, setShowSuccessModal, showEditForm, setShowEditForm}) {
 
     const { fetchGeneral, getFetchGeneral } = useContext(FetchContext);  // handles fetch requests
-
+    const {login} = useContext(GlobalContext)
 
     const handleSubmitInfo = async (event) => {
         event.preventDefault()
@@ -36,10 +37,14 @@ return <>
                 </Modal.Header>
                 <Modal.Body>
                     <Form onSubmit={handleSubmitInfo} return="false">
-                        <input type="text" name="username" className="form-control mb-2" placeholder="Username" aria-label="username" required />
-                        <input type="text" name="name" className="form-control mb-2" placeholder="Name" aria-label="name" required />
-                        <input type="text" name="lastname" className="form-control mb-2" placeholder="Surname" aria-label="lastname" required />
-                        <input type="text" name="password" className="form-control mb-2" placeholder="Password" aria-label="password" required />
+                        <label>Username</label>
+                        <input type="text" name="username" className="form-control mb-2" placeholder="Username" aria-label="username" defaultValue={login && login.username && login.username} required />
+                        <label>Name</label>
+                        <input type="text" name="name" className="form-control mb-2" placeholder="Name" aria-label="name" defaultValue={login && login.name && login.name} required />
+                        <label>Surname</label>
+                        <input type="text" name="lastname" className="form-control mb-2" placeholder="Surname" aria-label="lastname" defaultValue={login && login.lastname && login.lastname} required/>
+                        <label>Password</label>
+                        <input type="password" name="password" className="form-control mb-2" placeholder="Password" aria-label="password" defaultValue={login && login.password} required />
                         <Button variant="secondary" className="my-3" onClick={() => setShowEditForm(false)}>
                             Cancel
                         </Button>
